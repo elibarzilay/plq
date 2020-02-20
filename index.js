285b8c14cf121c9b150a473f9f1c39832af3dd78
Eli Barzilay <eli@barzilay.org>
2020-02-20 10:51:05 -0500

    Major revision
    Main new feature: dealing better with multiple choice, and make button
    indicator that visually display a state of "selected" when clicked, and
    independently "confirmed" when receiving confirmation from the server.

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const multiNone     = "〈none〉";
 
 const $ = x => typeof x === "string" ? document.getElementById(x) : x;
 
-const sleep = ms => () => new Promise(res => setTimeout(res, ms));
+const rec = f => f((...xs) => rec(f)(...xs));
+
+const sleep = ms => ()=> new Promise(res => setTimeout(res, ms));
 
 const objQuery = o =>
   Object.keys(o).map(k => `${k}=${encodeURIComponent(o[k])}`).join("&")
@@ -18,35 +20,29 @@ const objQuery = o =>
 const evListener = (id, type, f) =>
   $(id).addEventListener(type, f, true);
 
-const show = (msg, kind, txt = null, editor = false) => {
+const show = (msg_css, txt, click) => {
+  const [msg, css] = Array.isArray(msg_css) ? msg_css : [msg_css, "send"];
   const messages = $("log");
   while (messages.children.length > 12) messages.lastChild.remove();
   const fst = document.createElement("div");
   fst.append(document.createTextNode(msg));
-  fst.classList.add(kind || "bad");
+  if (css) fst.classList.add(css);
   if (txt) {
     const snd = document.createElement("div");
     snd.classList.add("histtext");
     snd.append(document.createTextNode(txt));
-    snd.classList.add("reuse-text");
-    if (txt != passwordTest)
-      snd.addEventListener("click", () => {
-        if (editor != editorOn) toggleEditor();
-        curText.value = txt; curText.focus(); });
+    if (click) {
+      snd.classList.add("reuse-text");
+      snd.addEventListener("click", click);
+    }
     fst.append(snd);
   }
   messages.prepend(fst);
   return fst;
 };
-const makeSend = (msg, txt, editor) => show(msg, "send", txt, editor);
-const changeSend = (node, msg, kind) => {
-  node.classList.remove("send");
-  node.classList.add(kind || "bad");
-  if (typeof msg === "string") node.firstChild.nodeValue = msg;
-  else { node.firstChild.remove(); node.prepend(msg); }
-};
+const showError = msg => show([msg, "bad"]);
 
-const getImg = () => {
+const getImg = ()=> {
   const data = localStorage.getItem("plq-img");
   if (!data) return null;
   const img = new Image();
@@ -62,20 +58,23 @@ const setImg = txt => {
   else { localStorage.setItem("plq-img", txt); n.append(getImg()); }
 };
 
-const sendToLogger = (txt, ok = null, fail = null, editor = false) => {
+const send = (txt, ok, fail, click) => {
   if (txt == "") return;
   const req = new XMLHttpRequest();
-  const msg = makeSend("Sending...", txt, editor);
-  req.onreadystatechange = () => {
+  const msg = show("Sending...", txt, click);
+  req.onreadystatechange = ()=> {
     if (req.readyState != 4) return;
     const isImage = /^data:image\/png;base64,/.test(req.responseText);
     const reply = !isImage ? req.responseText
                            : (setImg(req.responseText), getImg());
-    console.log("--> %o", reply);
+    // console.log("--> %o", reply);
     const isOK = req.status == 200
               && (isImage || /^OK\b/.test(req.responseText));
-    changeSend(msg, reply, isOK && "ok");
-    if (isOK) setSelectedButtons(txt), ok && ok(); else fail && fail();
+    msg.classList.remove("send");
+    msg.classList.add(isOK ? "ok" : "bad");
+    if (typeof reply === "string") msg.firstChild.nodeValue = reply;
+    else { msg.firstChild.remove(); msg.prepend(reply); }
+    if (isOK) ok && ok(); else fail && fail();
   };
   const [user, pswd] = getLogin();
   req.open("GET", `/plq?${
@@ -83,14 +82,9 @@ const sendToLogger = (txt, ok = null, fail = null, editor = false) => {
   req.send();
 };
 
-class BSet extends Set {
-  toggle(val) { if (this.has(val)) this.delete(val); else this.add(val); }
-  addAll(vals) { vals.forEach(v => this.add(v)); }
-  toString() { return this.size ? [...this.values()].join("; ") : multiNone; }
-}
-
-let lastButtons = "";
-const setButtons = (bs = "") => {
+let lastButtons = "", lastRawButtons = "";
+const setButtons = (bs = lastRawButtons) => {
+  lastRawButtons = bs;
   let flags = "";
   { const m = bs.match(/^〈〈(.*)〉〉\n([\s\S]*)$/);
     if (m) { flags = m[1]; bs = m[2]; }
@@ -98,50 +92,51 @@ const setButtons = (bs = "") => {
   bs = bs.split("\n").map(s => s.trim()).filter(s => s.length);
   const isMulti = flags.includes("+");
   const isSudo = getUser() == sudoUser
-              && (lastButtons.length && lastButtons[0] == "*" ? true : "new");
+              && (lastButtons && lastButtons[0] == "*" ? true : "new");
   const newButtons = (isSudo ? "*" : "-") + "\n" + bs.join("\n");
-  if (newButtons == lastButtons) return; else lastButtons = newButtons;
+  if (bs && newButtons == lastButtons) return; else lastButtons = newButtons;
   const div = $("buttons");
   div.dataset.mode = isMulti ? "multi" : "";
-  const toSend = !isMulti ? b => b : (() => {
-    const multi = new BSet();
-    return b => ((b == multiNone ? multi.clear() :
-                  b == multiAll  ? multi.addAll(bs) :
-                  multi.toggle(b)),
-                 multi.toString());
-  })();
-  const mkBreak = () => {
-    const b = document.createElement("div");
-    b.classList.add("break");
+  let btns;
+  const clickText = b => {
+    const setall = f => btns.forEach(b => b.classList.toggle("selected", f(b)));
+    if (!isMulti) { setall(bb => b == bb); return b.innerText; }
+    else if (b.innerText == multiAll)  setall(b => true);
+    else if (b.innerText == multiNone) setall(b => false);
+    else b.classList.toggle("selected");
+    return btns.filter(b => b.classList.contains("selected"))
+               .map(b => b.innerText).join("; ") || multiNone;
+  };
+  const mkElement = (tag, cssClass, txt, isOp) => {
+    const b = document.createElement(tag);
+    b.classList.add(cssClass);
+    if (isOp) b.classList.add("op");
+    if (txt) b.innerText = txt;
     div.appendChild(b);
+    if (txt)
+      evListener(b, "click",
+                 isOp ? rec(me => ()=> send(opWrap(txt), null, null, me))
+                 : ()=> send(txt = clickText(b), ()=> setBtnStatus(txt), null,
+                             reuseText(txt, false)));
     return b;
   };
-  const mkButton = (txt, wrap = toSend) => {
-    const b = document.createElement("button");
-    b.classList.add("btn");
-    if (wrap === opWrap) b.classList.add("op");
-    b.innerText = txt;
-    div.appendChild(b);
-    evListener(b, "click", () => sendToLogger(wrap(txt)));
-    return b;
-  };
-  const equalizeWidths = bs => {
+  const mkBr    =   ()=> mkElement("div", "break");
+  const mkBtn   = txt => mkElement("button", "btn", txt);
+  const mkOpBtn = txt => mkElement("button", "btn", txt, true);
+  const equalWidths = bs => {
     const max = bs.map(b => b.getBoundingClientRect().width)
                   .reduce((x,y) => Math.max(x,y), 0);
     bs.forEach(b => b.style.width = max + "px");
+    return bs;
   };
-  const createElts = () => {
+  const createElts = ()=> {
     while (div.firstChild) div.firstChild.remove();
-    if (isSudo) {
-      equalizeWidths(opButtons.map(b => mkButton(b, opWrap)));
-      mkBreak();
-    }
-    equalizeWidths(bs.map(b => mkButton(b)));
-    if (isMulti) {
-      mkBreak();
-      equalizeWidths([mkButton(multiAll), mkButton(multiNone)]);
-    }
-    if (isSudo == "new") setTimeout(() => sendToLogger(opWrap("Status")), 250);
+    if (isSudo) { equalWidths(opButtons.map(mkOpBtn)); mkBr(); }
+    btns = equalWidths(bs.map(mkBtn));
+    if (isMulti) { mkBr(); equalWidths([multiAll, multiNone].map(mkBtn)); }
+    if (isSudo == "new")
+      [...buttons.querySelectorAll(".btn.op")]
+        .find(b => b.innerText.match(/Status/i)).click();
   };
   div.style.height = `${div.scrollHeight}px`;
   Promise.resolve()
@@ -151,16 +146,16 @@ const setButtons = (bs = "") => {
     .then(sleep(500)).then(()=> div.style.height = ``);
 };
 
-const setSelectedButtons = txt => {
+const setBtnStatus = (txt, css = "confirmed") => {
   const div = $("buttons");
   const bs = [...div.querySelectorAll(".btn:not(.op)")];
-  const setSel = (b, sel) => b.classList[sel ? "add" : "remove"]("selected");
+  const setSel = (b, sel) => b.classList.toggle(css, sel);
   if (div.dataset.mode != "multi")
     return bs.forEach(b => setSel(b, b.innerText == txt));
   const txts = txt == multiNone ? [] : txt.split(/ *; */);
-  let all = true, none = true;
+  let all = css == "confirmed", none = css == "confirmed";
   bs.forEach(b => { // relies on none/all being at the end
-    if (b.innerText == multiAll) setSel(b, all);
+    if      (b.innerText == multiAll)  setSel(b, all);
     else if (b.innerText == multiNone) setSel(b, none);
     else if (txts.includes(b.innerText)) { none = false; setSel(b, true);  }
     else                                 { all  = false; setSel(b, false); }
@@ -168,9 +163,15 @@ const setSelectedButtons = txt => {
 };
 
 let curText = $("thetext-line"), editorOn = false, editorModified = false;
+const reuseText = (txt, editor = editorOn) => ()=> {
+  if (editorOn != editor) toggleEditor();
+  curText.value = txt; curText.focus();
+};
 const theTextSend = (clear = !editorOn) => {
-  sendToLogger(curText.value.trim(), clear && (() => curText.value = ""),
-               null, editorOn);
+  const inEditor = editorOn, txt = curText.value.trim();
+  const onOK = ()=> { setBtnStatus(txt); if (clear) curText.value = ""; };
+  setBtnStatus(txt, "selected");
+  send(txt, onOK, null, reuseText(txt));
   curText.focus();
 };
 const theTextKeyListen = (id, editor) =>
@@ -190,27 +191,27 @@ const theTextKeyListen = (id, editor) =>
   });
 theTextKeyListen("thetext-line", false);
 theTextKeyListen("thetext-area", true);
-evListener("thetext-submit", "click", () => theTextSend());
-evListener("thetext-done",   "click", () => editorDone());
+evListener("thetext-submit", "click", ()=> theTextSend());
+evListener("thetext-done",   "click", ()=> editorDone());
 $("thetext-area").addEventListener("input", e => {
   if (editorModified) return;
   editorModified = true;
-  sendToLogger("New Question");
+  send("New Question");
 });
 
-const startWS = (() => {
+const startWS = (()=> {
   // The delay D between runs starts at MIN sec, and in each iteration,
   // then it goes to D*(STEPUP/T**STEPDN) for an execution that lasted T
   // secs, kept within the MIN..MAX bounds.
   const MIN = 1, MAX = 60, STEPUP = 1.5,
         STEPDN = STEPUP ** (1/30); // T = 30s => no change in delay
   let ws, start, delay = MIN;
-  return () => {
+  return ()=> {
     start = Date.now();
     ws = new WebSocket("wss://plq.barzilay.org/ws");
     ws.onmessage = e => setButtons(e.data);
     ws.onerror = e => console.error("websocket error", e);
-    ws.onclose = () => {
+    ws.onclose = ()=> {
       const elapsed = (Date.now() - start) / 1000;
       const mult    = STEPUP / (STEPDN ** elapsed);
       delay         = Math.max(Math.min(delay * mult, MAX), MIN);
@@ -220,39 +221,38 @@ const startWS = (() => {
   };
 })();
 
-const getLogin = () =>
+const getLogin = ()=>
   ["plq-user", "plq-pswd"].map(i => localStorage.getItem(i));
-const getUser = () => getLogin()[0];
-const setLogin = () => {
+const getUser = ()=> getLogin()[0];
+const setLogin = ()=> {
   const user = $("user").value.trim().toLowerCase(),
         pswd = $("pswd").value;
-  if (user == "") return show("missing username");
-  if (pswd == "") return show("missing password");
+  if (user == "") return showError("missing username");
+  if (pswd == "") return showError("missing password");
   $("user").value = user;
   $("pswd").value = "";
   localStorage.setItem("plq-user", user);
   localStorage.setItem("plq-pswd", md5(pswd));
   setImg(null);
-  sendToLogger(passwordTest,
-               () => { hideLogin();
-                       if (user == sudoUser) setButtons(); },
-               () => { localStorage.removeItem("plq-pswd");
-                       localStorage.removeItem("plq-user");
-                       setImg(null); });
+  send(passwordTest,
+       ()=> { hideLogin(); setButtons(); },
+       ()=> { localStorage.removeItem("plq-pswd");
+              localStorage.removeItem("plq-user");
+              setImg(null); });
 };
 if (getUser()) setImg(localStorage.getItem("plq-img"));
 
 let menuShown = false;
-const toggleMenu = () => {
+const toggleMenu = ()=> {
   menuShown = !menuShown;
-  $("nav-links").classList[menuShown ? "add" : "remove"]("expanded");
+  $("side-links").classList.toggle("expanded", menuShown);
   $("menu-handle")[menuShown ? "focus" : "blur"]();
 };
-const hideMenu = () => menuShown && toggleMenu();
+const hideMenu = ()=> menuShown && toggleMenu();
 evListener("menu-handle", "click", toggleMenu);
-evListener("menu-handle", "blur", () => setTimeout(hideMenu,100));
+evListener("menu-handle", "blur", ()=> setTimeout(hideMenu,100));
 
-const showLogin = () => {
+const showLogin = ()=> {
   $("text-container").style.display = "none";
   $("login-container").style.display = "block";
   const user = localStorage.getItem("plq-user");
@@ -271,7 +271,7 @@ const keyLogin = e => {
   switch (e.key) {
   case "Enter":  return setLogin();
   case "Escape": if (getUser()) return hideLogin();
-                 else return show("please enter your login information");
+                 else return showError("please enter your login information");
   }
 };
 evListener("show-login", "click", toggleLogin);
@@ -286,24 +286,24 @@ const resetEditor = (force = false) => {
   }
 };
 
-const toggleEditor = () => {
-  let focus = document.activeElement == curText;
+const toggleEditor = ()=> {
   if (!editorOn && getUser() != sudoUser) return;
+  let focus = document.activeElement == curText;
   editorOn = !editorOn;
   curText = $(editorOn ? "thetext-area" : "thetext-line");
-  document.body.classList[editorOn ? "add" : "remove"]("qedit");
+  document.body.classList.toggle("qedit", editorOn);
   if (focus) curText.focus();
   resetEditor();
 };
 
-const editorDone = () => {
+const editorDone = ()=> {
   if (!editorOn) return;
   theTextSend(true);
-  sendToLogger(opWrap("Done"), () => resetEditor(true));
+  send(opWrap("Done"), ()=> resetEditor(true));
 };
 
 let timerRunning = null, timerDeadline = null, timerShown = null;
-const timerUpdate = () => {
+const timerUpdate = ()=> {
   const show = Math.ceil((timerDeadline - Date.now()) / 1000);
   if (timerShown == show) return;
   const pad2 = n => n < 10 ? "0"+n : n;
@@ -322,7 +322,7 @@ const timerUpdate = () => {
     setText("!!!"+bzz[Math.min(Math.floor((-show-1)/2), bzz.length-1)]+"!!!");
   } else timerAdd(0)();
 };
-const timerAdd = d => () => {
+const timerAdd = d => ()=> {
   if (getUser() != sudoUser) return;
   const now = Date.now();
   timerDeadline = (timerDeadline || now) + d*1000;
@@ -332,14 +332,14 @@ const timerAdd = d => () => {
   tDiv.classList.remove("over");
   tDiv.style.opacity = 0;
   tDiv.style.width = "";
-  setTimeout(() => tDiv.style.opacity = 0.9, 100);
+  setTimeout(()=> tDiv.style.opacity = 0.9, 100);
   if (timerRunning) { clearInterval(timerRunning); timerRunning = null; }
   if (timerDeadline > now) timerRunning = setInterval(timerUpdate, 100);
   else timerDeadline = null;
 };
 
 const hotKeys = new Map([
-  ["t", () => curText.focus()],
+  ["t", ()=> curText.focus()],
   ["l", toggleLogin],
   ["s", toggleEditor],
   ["d", editorDone],
@@ -356,7 +356,7 @@ window.addEventListener("keyup", (e =>
   && (e.preventDefault(), e.stopImmediatePropagation(), hotKeys.get(e.key)())),
   true);
 
-window.addEventListener("load", () => {
+window.addEventListener("load", ()=> {
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register(
       window.location.href.replace(/\/[^\/]*$/, "/worker.js"))
@@ -372,7 +372,7 @@ window.addEventListener("beforeinstallprompt", e => {
   $("install").style.display = "block";
   installEvt = e;
 });
-evListener("install", "click", () => setTimeout(() => {
+evListener("install", "click", ()=> setTimeout(()=> {
   if (!installEvt) return;
   console.log("Installing...");
   $("install").style.display = "none";
